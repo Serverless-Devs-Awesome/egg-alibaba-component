3b92a7cf2f44b546bbaa17bae76c34c0e3cc4240
hanxie <hanxie.wq@alibaba-inc.com>
2021-03-22 14:08:52 +0800

    add lib files

---
+++ lib/bootstrap.d.ts
@@ -0,0 +1,4 @@
+export declare const DEFAULTBOOTSTRAP = "#!/usr/bin/env bash\n\nexport PORT={port}\n{start}";
+export declare const DEFAULTPORT = 9000;
+export declare const DEFAULTSTART = "export EGG_SERVER_ENV=prod\nnpx --no-install egg-scripts start --workers=1";
+//# sourceMappingURL=bootstrap.d.ts.map
